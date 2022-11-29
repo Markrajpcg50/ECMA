fa750d54c4452651ea391bcc924ab700273a56c7
Markrajpcg50 <112145938+Markrajpcg50@users.noreply.github.com>
2022-11-29 16:05:46 +0530

    completed with TypeScript

--- typescriptbasics.js
+++ typescriptbasics.js
@@ -1,5 +1,31 @@
 // we get errors in transpiler phase and compilation is done to .js file
 // 
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
 var a = "1";
 console.log(a);
 console.log("aaa", a);
@@ -50,3 +76,113 @@ var user5 = {
     }
 };
 console.log(user5.getMessage());
+;
+;
+// Union and type aliased 
+var username = "mohan";
+var pageName = "first"; // can have both string and number
+var errorMessage = null;
+var user6 = null;
+var someProp;
+var popularTags = ["dragon", "coffee"];
+var dragonsTag = null;
+// void in Typescript
+// void is undefined and null
+var doSomething = function () {
+    console.log("doSomething");
+    return;
+};
+var foo = "foo"; // avoid using any
+// never in TypeScript
+var doAnything = function () {
+    console.log("doAnything");
+    throw "never";
+};
+// unknown in TypeScript
+var vAny = 10;
+var vUnknown = "Mohan";
+var s1 = vAny;
+var s2 = vUnknown;
+//console.log(vAny.foo());
+//console.log(vUnknown.foo());
+// converting one type to another
+var pageNumber = "1";
+var numericpageNumber = pageNumber;
+// Working with DOM- document object module 
+var page = "1";
+var pageNum = page;
+pageNum = "1";
+var User3 = /** @class */ (function () {
+    function User3(firstName, lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    User3.prototype.changeUnchangeablename = function () {
+        //this.unchangableName="foo";
+    };
+    User3.prototype.getFullName = function () {
+        return this.firstName + " " + this.lastName;
+    };
+    User3.maxAge = 50;
+    return User3;
+}());
+var Admin = /** @class */ (function (_super) {
+    __extends(Admin, _super);
+    function Admin() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    Admin.prototype.setEditor = function (editor) {
+        this.editor = editor;
+    };
+    Admin.prototype.getEditor = function () {
+        return this.editor;
+    };
+    return Admin;
+}(User3));
+var user7 = new User3("Monster", "lessons");
+console.log(user7.unchangableName);
+console.log(User3.maxAge);
+var admin = new Admin("Foo", "Bar");
+console.log(admin.getFullName());
+admin.setEditor("VS-Code");
+console.log(admin.getEditor());
+// Generics in TypeScript
+//const updatedArray=append<string>("baz",["foo","bar"]);
+//const searchStr="foo"
+//const hasSearchString=any<string>((el:string)=>el.contains(searchStr),[
+//    "foo",
+//    "bar",
+//    "baz"
+//]);
+var addID = function (obj) {
+    var id = Math.random().toString(16);
+    return __assign(__assign({}, obj), { id: id });
+};
+var user8 = {
+    name: "jack",
+    data: {
+        meta: "foo"
+    },
+    meta: "bar"
+};
+var user9 = {
+    name: "Johan",
+    data: ["foo", "boo"],
+    meta: "guu"
+};
+var result = addID(user8);
+console.log("result", result);
+// Enums in Typescript
+var statuses = {
+    notStarted: 0,
+    inProgress: 1,
+    done: 2
+};
+console.log(statuses.inProgress);
+var Status;
+(function (Status) {
+    Status["NotStarted"] = "notStarted";
+    Status["InProgress"] = "inProgress";
+    Status["Done"] = "done";
+})(Status || (Status = {}));
+console.log(Status.InProgress);
--- typescriptbasics.ts
+++ typescriptbasics.ts
@@ -110,3 +110,148 @@ let someProp: String |number |undefined|string[]|object
 
 const popularTags:popularTag[]=["dragon","coffee"];
 
+const dragonsTag: MaybePopularTag=null;
+
+// void in Typescript
+// void is undefined and null
+const doSomething=(): void =>{
+    console.log("doSomething");
+    return 
+}
+const foo:any ="foo"; // avoid using any
+
+// never in TypeScript
+
+const doAnything=():never=>{
+    console.log("doAnything");
+    throw "never";
+}
+
+// unknown in TypeScript
+
+let vAny:any=10;
+let vUnknown:unknown="Mohan";
+let s1:string=vAny;
+let s2:string=vUnknown as string;
+
+//console.log(vAny.foo());
+//console.log(vUnknown.foo());
+
+// converting one type to another
+
+let pageNumber:string="1"
+let numericpageNumber:number=(pageNumber as unknown) as number;
+ 
+// Working with DOM- document object module 
+let page:any="1";
+let pageNum=page as string;
+pageNum="1";
+
+//const someElement=document.querySelector(".foo") as HTMLInputElement;
+
+//console.log('someElement',someElement.value);
+//someElement.addEventListener("blur",(event)=>{
+//    console.log('event',event.target.value);
+//})
+
+// Classes and Interfaces in TypeScript
+
+interface UserInterface2{
+    getFullName():string;
+}
+class User3 implements UserInterface2 {
+    private firstName: string
+    private lastName: string
+    readonly unchangableName:string;
+    static readonly maxAge =50;
+    constructor(firstName:string, lastName:string){
+        this.firstName=firstName;
+        this.lastName=lastName;
+    }
+    changeUnchangeablename():void{
+        //this.unchangableName="foo";
+    }
+    getFullName():string{
+        return this.firstName+" "+this.lastName
+    }
+}
+
+class Admin extends User3{
+    private editor:string;
+    setEditor(editor:string):void{
+        this.editor=editor;
+    }
+    getEditor():string{
+        return this.editor;
+    }
+}
+
+const user7=new User3("Monster","lessons");
+console.log(user7.unchangableName);
+console.log(User3.maxAge);
+
+const admin= new Admin("Foo","Bar");
+console.log(admin.getFullName());
+admin.setEditor("VS-Code")
+console.log(admin.getEditor());
+
+// Generics in TypeScript
+
+//const updatedArray=append<string>("baz",["foo","bar"]);
+//const searchStr="foo"
+//const hasSearchString=any<string>((el:string)=>el.contains(searchStr),[
+//    "foo",
+//    "bar",
+//    "baz"
+//]);
+
+const addID= <T extends object>(obj:T)=>{
+    const id= Math.random().toString(16)
+    return{
+    ...obj,
+    id
+    }
+}
+
+interface UserInterface3<T,V>{
+    name:string,
+    data:T;
+    meta:V;
+}
+const user8:UserInterface3<{meta: string},string>={
+    name:"jack",
+    data:{
+        meta:"foo",
+    },
+    meta:"bar",
+}
+const user9:UserInterface3<string[],string>={
+    name:"Johan",
+    data:["foo","boo"],
+    meta:"guu",
+}
+const result= addID<UserInterface3<{meta:string},string>>(user8);
+console.log("result",result);
+
+// Enums in Typescript
+// can hold constants
+const statuses={
+    notStarted:0,
+    inProgress:1,
+    done:2
+};
+
+console.log(statuses.inProgress);
+
+enum StatusEnum{
+    NotStarted="notStarted",
+    InProgress="inProgress",
+    Done="done"
+}
+
+interface Task{
+    id:string;
+    status:StatusEnum;
+}
+
+console.log(Status.InProgress);
